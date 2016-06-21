736f49929294147fa6dda82821a33eccfa87018f
wudi <wudi2852987@163.com>
2016-06-21 14:31:04 +0800

    add page init

--- src/core/FUI.js
+++ src/core/FUI.js
@@ -5,7 +5,18 @@ define(['base', 'language', 'widget'], function (base, language, widget) {
     var Widget = widget.Widget;
 
 
-    var VERSION = '0.1.2';
+    var VERSION = '1.0.0';
+
+    var defaults = {
+        autoInit: false, //自动初始化页面
+        showPageLoadingIndicator: true, //push.js加载页面的时候显示一个加载提示
+        router: true, //默认使用router
+        swipePanel: "left", //滑动打开侧栏
+        swipePanelOnlyClose: true  //只允许滑动关闭，不允许滑动打开侧栏
+    };
+
+
+
 
     function FUI() {
         this.version = VERSION;
@@ -26,10 +37,10 @@ define(['base', 'language', 'widget'], function (base, language, widget) {
 
     var F = new FUI();
 
+    F.FUIConfig = F.extend(defaults, F.config);
+
     window.FUI = F;
 
-    return {
-        FUI: F
-    };
+    return F;
 
 });
